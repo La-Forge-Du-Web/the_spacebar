943e82a2bcf6d9419ec37bfe02a505db5d7d9d99
Malhju <laforgeduweb@gmail.com>
2019-01-26 19:52:32 +0100

    twig installation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,9 +21,14 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article : %s',
-            $slug
-        ));
+        $comments = [
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque enim incidunt minima provident consectetur quos! Necessitatibus, expedita pariatur! Veniam repellendus ipsam impedit asperiores, cum incidunt quasi commodi dolores itaque unde?',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur harum porro perferendis. Aperiam illum atque provident quos mollitia illo quaerat distinctio sunt quas, necessitatibus accusantium soluta aspernatur hic! Quia?',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
