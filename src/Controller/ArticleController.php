0f675ce5906ae68e8cb49dcb8b46fc4816cc74bb
Malhju <laforgeduweb@gmail.com>
2019-01-26 21:47:01 +0100

    profiler installation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -24,7 +24,10 @@ class ArticleController extends AbstractController
         $comments = [
             'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque enim incidunt minima provident consectetur quos! Necessitatibus, expedita pariatur! Veniam repellendus ipsam impedit asperiores, cum incidunt quasi commodi dolores itaque unde?',
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur harum porro perferendis. Aperiam illum atque provident quos mollitia illo quaerat distinctio sunt quas, necessitatibus accusantium soluta aspernatur hic! Quia?',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus harum est natus cum quae. Labore excepturi ipsa sapiente iste enim veritatis vel dicta distinctio voluptatibus, est molestiae iusto expedita provident.',
         ];
+
+        dump($slug, $this);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
